9d53d7eb8a619a5a79d5438cbd75ac9374ddc351
Mihuy1 <patrik.hyytiainen@protonmail.com>
2026-01-19 13:02:12 +0200

    cleanup workout history cards code

--- components/ui/workoutHistoryCard.tsx
+++ components/ui/workoutHistoryCard.tsx
@@ -1,6 +1,6 @@
 import { CompletedWorkout } from "@/app/(tabs)/explore";
-import { useEffect, useRef } from "react";
-import { Animated, FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import Animated from "react-native-reanimated";
 import { ThemedText } from "../themed-text";
 
 type WorkoutHistoryCardProps = {
@@ -15,97 +15,58 @@ export default function WorkoutHistoryCard({
   itemId,
 }: WorkoutHistoryCardProps) {
   const isExpanded = expandId === itemId;
-  const animatedHeight = useRef(new Animated.Value(0)).current;
-  const animatedOpacity = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    if (isExpanded) {
-      // Expand animation
-      Animated.parallel([
-        Animated.timing(animatedHeight, {
-          toValue: 1,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-        Animated.timing(animatedOpacity, {
-          toValue: 1,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-      ]).start();
-    } else {
-      // Collapse animation
-      Animated.parallel([
-        Animated.timing(animatedHeight, {
-          toValue: 0,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-        Animated.timing(animatedOpacity, {
-          toValue: 0,
-          duration: 200,
-          useNativeDriver: false,
-        }),
-      ]).start();
-    }
-  }, [isExpanded]);
-
-  const renderExpandedExercise = ({
-    item: exercise,
-  }: {
-    item: CompletedWorkout["exercises"][0];
-  }) => (
-    <View style={styles.exerciseWrapper}>
-      <Text style={styles.exerciseName}>{exercise.name}</Text>
-      <View style={styles.tableHeader}>
-        <Text style={[styles.cell, styles.setCol]}>SET</Text>
-        <Text style={[styles.cell, styles.kgCol]}>WEIGHT & REPS</Text>
-      </View>
-
-      <View>
-        {exercise.sets.map((set, index) => (
-          <View
-            key={set.id}
-            style={[
-              styles.tableRow,
-              index % 2 === 0 ? styles.evenRow : styles.oddRow,
-            ]}
-          >
-            <ThemedText type="default" style={[styles.cell, styles.setCol]}>
-              {index + 1}
-            </ThemedText>
-            <ThemedText type="default" style={[styles.cell, styles.kgCol]}>
-              {set.weight} kg x {set.reps} reps
-            </ThemedText>
-          </View>
-        ))}
-      </View>
-    </View>
-  );
-
-  const renderUnExpandedExercise = ({
-    item: exercise,
-  }: {
-    item: CompletedWorkout["exercises"][0];
-  }) => (
-    <View style={styles.exerciseWrapper}>
-      <View>
-        <Text>
-          {exercise.sets.length} sets of {exercise.name}
-        </Text>
-      </View>
-    </View>
-  );
 
   return (
-    <FlatList
-      data={exercises}
-      keyExtractor={(item, index) => item.name + index}
-      renderItem={
-        isExpanded ? renderExpandedExercise : renderUnExpandedExercise
-      }
-      scrollEnabled={false} // Disable to avoid nested scroll issues
-    />
+    <Animated.View>
+      <FlatList
+        data={exercises}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item: exercise }) => (
+          <View>
+            {!isExpanded ? (
+              <View>
+                <Text>
+                  {exercise.sets.length} sets of {exercise.name}
+                </Text>
+              </View>
+            ) : (
+              <View style={styles.exerciseWrapper}>
+                <Text style={styles.exerciseName}>{exercise.name}</Text>
+                <View style={styles.tableHeader}>
+                  <Text style={[styles.cell, styles.setCol]}>SET</Text>
+                  <Text style={[styles.cell, styles.kgCol]}>WEIGHT & REPS</Text>
+                </View>
+
+                <View>
+                  {exercise.sets.map((set, index) => (
+                    <View
+                      key={set.id}
+                      style={[
+                        styles.tableRow,
+                        index % 2 === 0 ? styles.evenRow : styles.oddRow,
+                      ]}
+                    >
+                      <ThemedText
+                        type="default"
+                        style={[styles.cell, styles.setCol]}
+                      >
+                        {index + 1}
+                      </ThemedText>
+                      <ThemedText
+                        type="default"
+                        style={[styles.cell, styles.kgCol]}
+                      >
+                        {set.weight} kg x {set.reps} reps
+                      </ThemedText>
+                    </View>
+                  ))}
+                </View>
+              </View>
+            )}
+          </View>
+        )}
+      />
+    </Animated.View>
   );
 }
 
